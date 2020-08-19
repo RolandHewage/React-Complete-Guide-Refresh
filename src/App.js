d9675ee34b6d8a739dd2c96853ae605af90af66d
RolandHewage <roland.hewage@gmail.com>
2020-08-19 23:10:29 +0530

    Setting styles dynamically

--- src/App.js
+++ src/App.js
@@ -85,7 +85,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -118,6 +119,7 @@ class App extends Component {
             age={this.state.persons[2].age} /> */}
         </div>
       );
+      style.backgroundColor = 'red';
     }
 
     return (
@@ -150,6 +152,7 @@ class App extends Component {
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
+
   }
 }
 
